8d5b3e21b80dee2131c0f661023ff46851472604
ramu <Murali_Athota@ad.infosys.com>
2025-02-05 17:20:43 +0100

    subscription update

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -3,16 +3,18 @@ resource "azurerm_resource_group" "demo" {
   location = "West Europe"
 }
 
-##  Demo now
+# Generate a random suffix
+resource "random_id" "storage_suffix" {
+  byte_length = 4
+}
+
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "satestant000012"
+  # The storage account name must be between 3 and 24 characters, using only lowercase letters and numbers.
+  name                     = "satestant${random_id.storage_suffix.hex}"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    video = "azure"
-    channel = "CloudQuickLabs"
-  }
+  account_replication_type = "LRS"
+  
+  # Other settings...
 }
